c7f1fba80f99c4e298569812541d6f8c457963ea
Leo42night <109854127+Leo42night@users.noreply.github.com>
2025-01-13 03:10:46 +0700

    migrate .sql & upload/ template file

--- app/Controllers/Serikats.php
+++ app/Controllers/Serikats.php
@@ -81,23 +81,37 @@
                 $fields = [
                     "name"=>[
                         'validator' => v::stringType()->notEmpty()->length(1, 40),
-                        'message' => 'Nama harus diisi dan antara 1 hingga 40 karakter.'
+                        'message' => 'Nama harus diisi antara 1 hingga 40 karakter'
                     ]
                 ];
+
+                // cek perubahan nama dan logo
                 $stmt = $this->db->prepare("SELECT logoPath FROM serikat WHERE id = ?");
                 $stmt->execute([$id]);
                 $serikat = $stmt->fetch();
-                if ($serikat && !empty($serikat['logoPath']) && file_exists($serikat['logoPath'])) {
-                    unlink($serikat['logoPath']);
-                } 
-                $dataValidate = Validation::ValidatorInput($fields,"index.php?page=serikat-edit");
-                $file = Validation::ValidatorFile($_FILES["logoPath"],"uploads/serikat/","index.php?page=serikat-edit&id=$id");
+
+                // lakukan validasi jika nama berubah
+                if ($serikat['name'] != $_POST['name']) {
+                    $serikat['name'] = Validation::ValidatorInput($fields,"index.php?page=serikat-edit&id=$id")["name"];
+                }
+
+                // lakukan validasi jika ada logo di up
+                if (!empty($_FILES["logoPath"]["name"])) {
+                    // hapus logo lama jika ada gambar baru
+                    if (file_exists($serikat['logoPath'])) {
+                        unlink($serikat['logoPath']);
+                    }
+
+                    // validasi dan masukkan nama file baru
+                    $serikat["logoPath"] = Validation::ValidatorFile($_FILES["logoPath"],"uploads/serikat/","index.php?page=serikat-edit&id=$id");
+                    
+                }
 
                 $updatedAt = date('Y-m-d H:i:s');
                 
                 $query = "UPDATE serikat SET name = ?, logoPath = ?, updateAt = ? WHERE id = ?";
                 $stmt = $this->db->prepare($query);
-                $success = $stmt->execute([$dataValidate["name"],$file,$updatedAt,$id]);
+                $success = $stmt->execute([$serikat["name"],$serikat["logoPath"],$updatedAt,$id]);
                 if ($success) {
                     $_SESSION['message'] = ['type' => 'success', 'text' => 'Sukses Mengubah Serikat  baru!'];
                     header('Location: index.php?page=serikat');
