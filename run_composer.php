cf0364be470a89bce6c5c8ef264f568dd1c31fb9
Leo42night <109854127+Leo42night@users.noreply.github.com>
2025-01-14 23:03:50 +0700

    now use pcntl_exec, it exist on server

--- run_composer.php
+++ run_composer.php
@@ -8,8 +8,10 @@ if (isset($_GET['token']) && $_GET['token'] === $secret) {
 function check_shell_function($function_name) {
   if (function_exists($function_name)) {
       echo "Fungsi '$function_name' tersedia.\n";
+      return true;
   } else {
       echo "Fungsi '$function_name' TIDAK tersedia.\n";
+      return false;
   }
 }
 
@@ -25,16 +27,24 @@ check_shell_function('putenv');
 
   // $composerPath = '/usr/local/bin/composer'; // Sesuaikan dengan lokasi composer
   // $command = $composerPath . ' install --no-dev --prefer-dist --no-progress 2>&1';
+  // Tentukan perintah yang ingin dijalankan
+  $command = '/usr/local/bin/composer';
+  $arguments = ['install', '--no-dev', '--prefer-dist', '--no-progress'];
 
-  // // Menggunakan shell_exec
-  // $output = passthru($command);
+  // Gantikan proses PHP dengan perintah composer
+  if(check_shell_function('pcntl_exec')) {
+    pcntl_exec($command, $arguments);
+  }
 
-  // if ($output) {
-  //     echo "Composer install berhasil dijalankan:\n";
-  //     echo "<pre>" . $output . "</pre>";
-  // } else {
-  //     echo "Terjadi kesalahan saat menjalankan Composer install.";
-  // }
+  // Menggunakan shell_exec
+  $output = passthru($command);
+
+  if ($output) {
+      echo "Composer install berhasil dijalankan:\n";
+      echo "<pre>" . $output . "</pre>";
+  } else {
+      echo "Terjadi kesalahan saat menjalankan Composer install.";
+  }
 } else {
   echo "Token tidak valid atau tidak ada.\n";
 }
